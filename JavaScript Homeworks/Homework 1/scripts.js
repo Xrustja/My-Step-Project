a0181770c225e960da8a74929ebe28ce1f642020
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2020-07-26 19:55:06 +0300

    HW1 JS

--- JavaScript Homeworks/Homework 1/scripts.js
+++ JavaScript Homeworks/Homework 1/scripts.js
@@ -1,6 +1,10 @@
 let name = prompt("Enter your name");
-let age = +prompt("Enter your age");
+let age = prompt("Enter your age");
 
+while (name === null || age === null || name.trim() === '' || age.trim() === '' || isNaN(+age)) {
+    name = prompt("Enter your name", name);
+    age = prompt("Enter your age", age);
+}
 if (age < 18) {
     alert('You are not allowed to visit this website.');
 } else if (age >= 18 && age <= 22) {
@@ -12,5 +16,4 @@ if (age < 18) {
 } else {
     alert('Welcome,' + name);
 }
-
 
