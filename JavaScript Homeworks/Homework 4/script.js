5ce585658dc2020b270d598b0c54620b5375bee4
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2020-08-04 20:25:26 +0300

    Done

--- JavaScript Homeworks/Homework 4/script.js
+++ JavaScript Homeworks/Homework 4/script.js
@@ -1,19 +1,69 @@
 function createNewUser(first_name, last_name) {
     const newUser = {
-        firstName: first_name || ' ',
-        lastName: last_name  || ' ',
         getLogin: function () {
-            let login = this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
-            return login
+            return this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
+        },
+        setFirstName: function (name) {
+            Object.defineProperty(
+                this,
+                "firstName",
+                {
+                    value: name,
+                    writable: false,
+                });
+        },
+        setLastName: function (last_name) {
+            Object.defineProperty(
+                this,
+                "lastName",
+                {
+                    value: last_name,
+                    writable: false,
+                });
         }
-           }
-
-    return newUser
+    };
+    Object.defineProperty(
+        newUser,
+        "firstName",
+        {
+            value: first_name,
+            writable: false,
+            configurable: true
+        });
+    Object.defineProperty(
+        newUser,
+        "lastName",
+        {
+            value: last_name,
+            writable: false,
+            configurable: true
+        });
+    return newUser;
 }
 
 let firstName = prompt('Enter your first name');
 let secondName = prompt('Enter your second name');
 
-const user = createNewUser(firstName,secondName);
+const user = createNewUser(firstName, secondName);
 console.log(user);
 console.log('Your login :' + user.getLogin());
+
+
+// Основное задание
+
+// function createNewUser(first_name, last_name) {
+//     return {
+//         firstName: first_name || ' ',
+//         lastName: last_name || ' ',
+//         getLogin: function () {
+//             return this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase()
+//         }
+//     }
+// }
+//
+// let firstName = prompt('Enter your first name');
+// let secondName = prompt('Enter your second name');
+//
+// const user = createNewUser(firstName,secondName);
+// console.log(user);
+// console.log('Your login :' + user.getLogin());
