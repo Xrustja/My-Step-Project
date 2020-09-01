1b12fee6bf25b9baedd5e94c9aa6d6ed1cc5879f
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2020-09-01 19:16:31 +0300

    optional task some try

--- JavaScript Homeworks/Homework 12/scripts.js
+++ JavaScript Homeworks/Homework 12/scripts.js
@@ -10,17 +10,29 @@ function onReady() {
 function changeImg(img) {
     let i = 1;
     let Interval = setInterval(() => {
-        img.forEach(el => el.classList.add('hidden'));
-        img[i].classList.remove('hidden');
+        // console.log('i new iteration =', i);
+        img.forEach(el => el.classList.remove('fadeOut', 'fadeIn'));
+        if (i === 0) {
+            img[img.length-1].classList.add('fadeOut');
+        } else {
+            img[i - 1].classList.add('fadeOut');
+        }
+        img[i].classList.add('fadeIn');
+        // console.log('i=', i);
+        // console.log('i-1=', i - 1);
         i < img.length - 1 ? i++ : i = 0;
-    }, 1000);
+        // console.log('i after check= ', i);
+    }, 2000);
 
     const stopBtn = document.getElementById('stop-btn');
-    stopBtn.onclick  = function () { clearInterval(Interval);
-        continueBtn.disabled = false;};
+    stopBtn.onclick = function () {
+        clearInterval(Interval);
+        continueBtn.disabled = false;
+    };
 
     const continueBtn = document.getElementById('continue-btn');
-    continueBtn.onclick = function () {changeImg(img);
+    continueBtn.onclick = function () {
+        changeImg(img);
         continueBtn.disabled = true;
     };
 
