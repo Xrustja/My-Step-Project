2f405eb73cfa0bd1f85d2a570f6dd2b935034c2c
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2020-09-07 01:09:17 +0300

    version 3

--- Step Project/scripts.js
+++ Step Project/scripts.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
     let tbsFilter = $('li.amazing-work-tabs-title');
     tbsFilter.click(function (event) {
-        tbsFilter.removeClass('amazing-work-tabs-title-border');
+            tbsFilter.removeClass('amazing-work-tabs-title-border');
             let targetTab = $(event.target);
             let imageBlock = $('div.two-images-container');
             targetTab.addClass('amazing-work-tabs-title-border');
@@ -37,10 +37,62 @@ $(document).ready(function () {
                 })
             }
         }
-    )
+    );
 
 
+    let images = [{img: "Images/Step Project Ham/graphic design/graphic-design7.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/graphic design/graphic-design8.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/graphic design/graphic-design9.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/graphic design/graphic-design10.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/graphic design/graphic-design11.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/graphic design/graphic-design12.jpg", type: "graphic-design"},
+        {img: "Images/Step Project Ham/landing page/landing-page5.jpg", type: "landing-pages"},
+        {img: "Images/Step Project Ham/landing page/landing-page6.jpg", type: "landing-pages"},
+        {img: "Images/Step Project Ham/landing page/landing-page7.jpg", type: "landing-pages"},
+        {img: "Images/Step Project Ham/wordpress/wordpress5.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/wordpress/wordpress6.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/wordpress/wordpress7.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/wordpress/wordpress8.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/wordpress/wordpress9.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/wordpress/wordpress10.jpg", type: "wordpress"},
+        {img: "Images/Step Project Ham/web design/web-design4.jpg", type: "web-design"},
+        {img: "Images/Step Project Ham/web design/web-design5.jpg", type: "web-design"},
+        {img: "Images/Step Project Ham/web design/web-design6.jpg", type: "web-design"},
+        {img: "Images/Step Project Ham/web design/web-design7.jpg", type: "web-design"}];
 
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    $("#amazing-work-btn").click(function () {
+        shuffle(images);
+        for (let i = 0; i < 12; i++) {
+            let image = images[i];
+            let src = image.img;
+            let type = image.type;
+            console.log(type);
+            console.log(src);
+            let newImageBlock = `<div class="two-images-container" data-type="${type}">
+                    <img class="amazing-work-img-border"
+                            src="${src}" alt="Graphic Design">
+                    <div class="amazing-work-img-flip-side">
+                        <img src="Images/Step%20Project%20Ham/logo/icon.png" alt="icon">
+                        <p class="flip-side-title">creative design</p>
+                        <p class="flip-side-type-name">${type}</p>
+                    </div>
+                </div>`;
+            console.log(newImageBlock);
+            let containerForAdd = document.querySelector('.amazing-work-img-container');
+            containerForAdd.insertAdjacentHTML("beforeend", newImageBlock);
+        $("li.amazing-work-tabs-title-border").click();
+        }
+        $("#amazing-work-btn").addClass("hidden");
+
+    })
 
 
 });
