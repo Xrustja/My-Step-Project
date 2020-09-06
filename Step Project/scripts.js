27e29fd2c4c97004d12ffa474b4e511129c7122e
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2020-09-06 22:53:32 +0300

    Last version

--- Step Project/scripts.js
+++ Step Project/scripts.js
@@ -1,19 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-        let tabs = document.querySelectorAll('li.our-services-tabs-title');
-        let text = document.querySelectorAll('div.our-service-content-container');
+$(document).ready(function () {
 
-    let activeTab;
-        let activeText;
-        tabs.forEach((tab, index) => {
-            tab.addEventListener('click', () => {
-                if (activeTab) activeTab.classList.toggle('active');
-                tab.classList.toggle('active');
-                activeTab = tab;
-                if (activeText) activeText.classList.toggle('active');
-                text[index].classList.toggle('active');
-                activeText = text[index];
-            })
-        });
+    let tabs = $("li.tabs-title");
+    let text = $("div.our-service-content-container");
+    let activeTab = $("li.tabs-title.active");
+    let activeText = $("div.our-service-content-container.active");
 
-    }
-);
+    $.each(tabs, function (index, tab) {
+        $(tab).click(function () {
+            if (activeTab.length) {
+                activeTab.toggleClass("active");
+                $(this).toggleClass("active");
+                activeTab = $(tab);
+            }
+            if (activeText.length) {
+                activeText.toggleClass("active");
+                $(text[index]).toggleClass("active");
+                activeText = $(text[index]);
+            }
+        })
+    });
+
+    let tbsFilter = $('li.amazing-work-tabs-title');
+    tbsFilter.click(function (event) {
+        tbsFilter.removeClass('amazing-work-tabs-title-border');
+            let targetTab = $(event.target);
+            let imageBlock = $('div.two-images-container');
+            targetTab.addClass('amazing-work-tabs-title-border');
+            if (targetTab.attr('data-type') === "all") {
+                imageBlock.removeClass('hidden');
+            } else {
+                imageBlock.removeClass('hidden');
+                $.each(imageBlock, function (index, img) {
+                    if (targetTab.attr('data-type') !== $(img).attr('data-type')) {
+                        $(imageBlock[index]).addClass('hidden')
+                    }
+                })
+            }
+        }
+    )
+
+
+
+
+
+});
